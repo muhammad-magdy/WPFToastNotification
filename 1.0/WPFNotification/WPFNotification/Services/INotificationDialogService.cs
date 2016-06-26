477ce1820e5d751e27fbe202231f16e0748f369a
mohamed magdy <muhammed.majdi@gmail.com>
2016-06-27 00:51:41 +0200

    Clear notification feature
    add clear notification feature that remove all notifications from
    notification list and buffer list.

--- 1.0/WPFNotification/WPFNotification/Services/INotificationDialogService.cs
+++ 1.0/WPFNotification/WPFNotification/Services/INotificationDialogService.cs
@@ -16,5 +16,10 @@ namespace WPFNotification.Services
         /// <param name="content">The notification object.</param>
         /// <param name="configuration">The notification configuration object.</param>
         void ShowNotificationWindow(object content, NotificationConfiguration configuration);
+
+        /// <summary>
+        /// Remove all notifications from notification list and buffer list.
+        /// </summary>
+        void ClearNotifications();
     }
 }
